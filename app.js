30d1c25080a3119cd2ddc764d42432c6b0e17dab
SwaatiPandey <swati.pandey@contentstack.com>
2021-01-14 18:35:00 +0530

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const router = require("./routes/blogRoutes");
 const { config } = require("process");
+const cors =require("cors");
+app.use(cors());
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
